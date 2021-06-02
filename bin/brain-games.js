192fe59d44d43e42483869fb1a375e589ba5fbb5
Maskane <kabanovmks@gmail.com>
2021-06-03 00:01:48 +0300

    2

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import { name } from '../src/cli.js';
 console.log('Welcome to the Brain Games!');
+ console.log(`Hello ${name}!`);
+
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync';
+
+export const name = readlineSync.question('Your answer: ');
